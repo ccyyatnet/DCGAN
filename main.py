8a5c7613e8e8a627bd96299183569a696f9540a9
ccyyatnet <1124015396@qq.com>
2016-12-20 03:11:52 +0000

    add py

---
+++ main.py
@@ -0,0 +1,90 @@
+import os
+import scipy.misc
+import numpy as np
+
+from model import DCGAN
+from utils import pp, visualize, to_json
+
+import tensorflow as tf
+
+tf.app.flags.DEFINE_string("devices", "gpu:3", "Which gpu to be used")
+
+tf.app.flags.DEFINE_string("dataset", "mnist", "The name of dataset [celebA, mnist, lsun]")
+tf.app.flags.DEFINE_integer("image_size", 64, "The size of the output images to produce [64]")
+tf.app.flags.DEFINE_integer("center_crop_size", 0, "The width of the images presented to the model, 0 for auto")
+tf.app.flags.DEFINE_boolean("is_crop", True, "True for training, False for testing [False]")
+tf.app.flags.DEFINE_integer("train_size", np.inf, "The size of train images [np.inf]")
+
+tf.app.flags.DEFINE_string("dir_tag", "tanhforminist", "dir_tag for sample_dir and checkpoint_dir")
+tf.app.flags.DEFINE_string("result_dir", "/home/zmzhou/Desktop/Workspace/result/", "Where to save the checkpoint and sample")
+tf.app.flags.DEFINE_boolean("is_train", True, "True for training, False for testing [False]")
+tf.app.flags.DEFINE_boolean("visualize", False, "True for visualizing, False for nothing [False]")
+tf.app.flags.DEFINE_boolean("b_loadcheckpoint", False, "b_loadcheckpoint")
+
+tf.app.flags.DEFINE_integer("epoch", 25, "Epoch to train [25]")
+tf.app.flags.DEFINE_float("beta1", 0.5, "Momentum term of adam [0.5]")
+tf.app.flags.DEFINE_float("d_learning_rate", 0.0002, "Learning rate of for adam")
+tf.app.flags.DEFINE_float("g_learning_rate", 0.0002, "Learning rate of for adam")
+
+tf.app.flags.DEFINE_integer("batch_size", 128, "The size of batch images")
+tf.app.flags.DEFINE_integer("gf_dim", 128, "gf_dim")
+tf.app.flags.DEFINE_integer("df_dim", 64, "df_dim")
+tf.app.flags.DEFINE_integer("dfc_dim", 1024, "df_dim")
+tf.app.flags.DEFINE_integer("gfc_dim", 1024, "df_dim")
+tf.app.flags.DEFINE_integer("z_dim", 100, "z_dim")
+tf.app.flags.DEFINE_integer("c_dim", 3, "c_dim")
+
+tf.app.flags.DEFINE_integer("K_for_Dtrain", 2, "K_for_Dtrain")
+tf.app.flags.DEFINE_integer("K_for_Gtrain", 1, "K_for_Gtrain") # Run g_optim twice to make sure that d_loss does not go to zero (different from paper)
+
+tf.app.flags.DEFINE_integer("num_classes", 1, "num_classes")
+tf.app.flags.DEFINE_float("class_loss_weight", 0.0, "The weight of class loss, in discriminator")
+tf.app.flags.DEFINE_float("d_label_smooth", 0.0, "The amount label smooth")
+tf.app.flags.DEFINE_float("generator_target_prob", 1.0, "The generator target prob")
+tf.app.flags.DEFINE_float("d_noise_image_mean", 0.0, "d_noise_image_mean")
+tf.app.flags.DEFINE_float("d_noise_image_var", 0.1, "d_noise_image_var")
+tf.app.flags.DEFINE_float("out_init_b", -0.45, "out_init_b")
+tf.app.flags.DEFINE_float("out_stddev", 0.075, "out_stddev")
+tf.app.flags.DEFINE_boolean("use_vbn", False, "True for use_vbn")
+tf.app.flags.DEFINE_boolean("minibacth", False, "True for minibacth")
+tf.app.flags.DEFINE_boolean("add_hz", False, "True for add random z in each hidden layer, in generator")
+
+FLAGS = tf.app.flags.FLAGS
+
+def main(_):
+
+    pp.pprint(FLAGS.__flags)
+
+    FLAGS.is_grayscale = (FLAGS.c_dim == 1)
+    FLAGS.sample_dir = FLAGS.result_dir + 'samples/' + FLAGS.dataset + '_' + FLAGS.dir_tag
+    FLAGS.checkpoint_dir = FLAGS.result_dir + 'checkpoint/' + FLAGS.dataset + '_' + FLAGS.dir_tag
+
+    if not os.path.exists(FLAGS.checkpoint_dir):
+        os.makedirs(FLAGS.checkpoint_dir)
+    if not os.path.exists(FLAGS.sample_dir):
+        os.makedirs(FLAGS.sample_dir)
+
+    config = tf.ConfigProto(allow_soft_placement=True, log_device_placement=False)
+    config.gpu_options.allow_growth = True
+
+    with tf.Session(config=config) as sess:
+
+        if FLAGS.dataset == 'mnist':
+            FLAGS.image_size = 32
+            FLAGS.c_dim = 1
+
+        with tf.device(FLAGS.devices):
+
+            dcgan = DCGAN(sess, config=FLAGS)
+
+            if FLAGS.is_train:
+                dcgan.train(FLAGS)
+            else:
+                dcgan.load(FLAGS)
+
+        #if FLAGS.visualize:
+        #    OPTION = 2
+        #   visualize(sess, dcgan, FLAGS, OPTION)
+
+if __name__ == '__main__':
+    tf.app.run()
---
+++ model.py
@@ -0,0 +1,349 @@
+from __future__ import division
+
+import os
+import time
+from glob import glob
+
+from ops import *
+from utils import *
+
+class DataProvider(object):
+    def __init__(self, config):
+
+        if config.dataset == 'mnist':
+            self.data_X, self.data_y = self.load_mnist()
+            self.len = len(self.data_X)
+        elif config.dataset == 'imagenet':
+            self.data = glob(os.path.join("/home/zmzhou/Desktop/Workspace/dataset/imagenet/ILSVRC2012/ILSVRC2012_img_train_t3/n*/*.JPEG"))
+            print 'data len:', len(self.data)
+            np.random.shuffle(self.data)
+            self.len = len(self.data)
+        else:
+            self.data = glob(os.path.join("/home/zmzhou/Desktop/Workspace/dataset/auto/", config.dataset, "*.jpg"))
+            print 'data len:', len(self.data)
+            np.random.shuffle(self.data)
+            self.len = len(self.data)
+
+    def load_data(self, config, idx):
+
+        if idx == 0:
+            if config.dataset == 'mnist':
+                seed = np.random.randint(10000)
+                np.random.seed(seed)
+                np.random.shuffle(self.data_X)
+                np.random.seed(seed)
+                np.random.shuffle(self.data_y)
+            else:
+                np.random.shuffle(self.data)
+
+        if config.dataset == 'mnist':
+            batch_images = self.data_X[idx * config.batch_size:(idx + 1) * config.batch_size]
+            batch_labels = self.data_y[idx * config.batch_size:(idx + 1) * config.batch_size]
+        else:
+            batch_files = self.data[idx * config.batch_size:(idx + 1) * config.batch_size]
+            batch_images = [get_image(batch_file, config.center_crop_size, is_crop=config.is_crop, resize_w=config.image_size, is_grayscale=config.is_grayscale) for batch_file in
+                            batch_files]
+            if (config.is_grayscale):
+                batch_images = np.array(batch_images).astype(np.float32)[:, :, :, None]
+            else:
+                batch_images = np.array(batch_images).astype(np.float32)
+            #batch_labels = np.zeros([config.batch_size, config.y_dim])
+
+        return batch_images  # , batch_labels
+
+    def blit(self, dest, src, loc):
+        pos = [i if i >= 0 else None for i in loc]
+        neg = [-i if i < 0 else None for i in loc]
+        target = dest[[slice(i, None) for i in pos]]
+        src = src[[slice(i, j) for i, j in zip(neg, target.shape)]]
+        target[[slice(None, i) for i in src.shape]] = src
+        return dest
+
+    def load_mnist(self, loadTest = False, useX32 = True):
+
+        data_dir = '/home/zmzhou/Desktop/Workspace/dataset/auto/mnist/'  # os.path.join("./data", config.dataset)
+
+        fd = open(os.path.join(data_dir, 'train-images-idx3-ubyte'))
+        loaded = np.fromfile(file=fd, dtype=np.uint8)
+        trX = loaded[16:].reshape((60000, 28, 28, 1)).astype(np.float)
+
+        fd = open(os.path.join(data_dir, 'train-labels-idx1-ubyte'))
+        loaded = np.fromfile(file=fd, dtype=np.uint8)
+        trY = loaded[8:].reshape((60000)).astype(np.float)
+
+        if loadTest:
+            fd = open(os.path.join(data_dir, 't10k-images-idx3-ubyte'))
+            loaded = np.fromfile(file=fd, dtype=np.uint8)
+            teX = loaded[16:].reshape((10000, 28, 28, 1)).astype(np.float)
+
+            fd = open(os.path.join(data_dir, 't10k-labels-idx1-ubyte'))
+            loaded = np.fromfile(file=fd, dtype=np.uint8)
+            teY = loaded[8:].reshape((10000)).astype(np.float)
+
+            trY = np.asarray(trY)
+            teY = np.asarray(teY)
+
+            X = np.concatenate((trX, teX), axis=0)
+            Y = np.concatenate((trY, teY), axis=0)
+        else:
+            X = trX
+            Y = trY
+
+        seed = 547
+        np.random.seed(seed)
+        np.random.shuffle(X)
+        np.random.seed(seed)
+        np.random.shuffle(Y)
+
+        y_vec = np.zeros((len(Y), 10), dtype=np.float)
+        for i, label in enumerate(Y):
+            y_vec[i, int(Y[i])] = 1.0
+
+        if useX32:
+            X32 = np.zeros([len(X), 32, 32, 1])
+            X32[:,2:30,2:30] = X
+            X = X32
+
+        return X / 127.5 - 1.0, y_vec
+
+    def read_and_decode(self, filename_queue):
+
+        reader = tf.TFRecordReader()
+        _, serialized_example = reader.read(filename_queue)
+        features = tf.parse_single_example(serialized_example, features={'image_raw': tf.FixedLenFeature([], tf.string), })
+
+        image = tf.decode_raw(features['image_raw'], tf.uint8)
+        image.set_shape(128 * 128 * 3)
+        image = tf.reshape(image, [128, 128, 3])
+
+        image = tf.cast(image, tf.float32) * (2. / 255) - 1.
+
+        return image
+
+    def read_and_decode_with_labels(self, filename_queue):
+
+        reader = tf.TFRecordReader()
+        _, serialized_example = reader.read(filename_queue)
+        features = tf.parse_single_example(serialized_example, features={'image_raw': tf.FixedLenFeature([], tf.string), 'label': tf.FixedLenFeature([], tf.int64)})
+
+        image = tf.decode_raw(features['image_raw'], tf.uint8)
+        image.set_shape(128 * 128 * 3)
+        image = tf.reshape(image, [128, 128, 3])
+
+        image = tf.cast(image, tf.float32) * (2. / 255) - 1.
+
+        label = tf.cast(features['label'], tf.int32)
+
+        return image, label
+
+
+class DCGAN(object):
+    def __init__(self, sess, config=None):
+
+        self.sess = sess
+        self.config = config
+
+        """
+        self.d_bn1 = batch_norm1(name='d_bn1')
+        self.d_bn2 = batch_norm1(name='d_bn2')
+        self.d_bn3 = batch_norm1(name='d_bn3')
+        self.g_bn0 = batch_norm1(name='g_bn0')
+        self.g_bn1 = batch_norm1(name='g_bn1')
+        self.g_bn2 = batch_norm1(name='g_bn2')
+        self.g_bn3 = batch_norm1(name='g_bn3')
+        """
+        self.build_model(config)
+
+    def build_model(self, config):
+
+        self.z = tf.placeholder(tf.float32, [config.batch_size, config.z_dim], name='z')
+        self.images = tf.placeholder(tf.float32, [config.batch_size] + [config.image_size, config.image_size, config.c_dim], name='real_images')
+
+        self.generate_image = self.generator(self.z, config=config)
+
+        self.probs_real, self.logits_real = self.discriminator(self.images, config=config)
+        self.probs_fake, self.logits_fake = self.discriminator(self.generate_image, reuse=True, config=config)
+
+        self.g_loss = tf.reduce_mean(tf.nn.sigmoid_cross_entropy_with_logits(self.logits_fake, tf.ones([config.batch_size,1])))
+        self.d_loss_fake = tf.reduce_mean(tf.nn.sigmoid_cross_entropy_with_logits(self.logits_fake, tf.zeros([config.batch_size,1])))
+        self.d_loss_real = tf.reduce_mean(tf.nn.sigmoid_cross_entropy_with_logits(self.logits_real, tf.ones([config.batch_size,1])))
+        self.d_loss = self.d_loss_real + self.d_loss_fake
+
+        self.total_loss = self.d_loss + self.g_loss
+        self.avg_prob_real = tf.reduce_mean(self.probs_real)
+        self.avg_prob_fake = tf.reduce_mean(self.probs_fake)
+
+        # tensorfolw summary:
+
+        tf.image_summary("generate_image", self.generate_image, 1000)
+
+        tf.histogram_summary("prob_real", self.probs_real)
+        tf.histogram_summary("prob_fake", self.probs_fake)
+
+        tf.scalar_summary("avg_probs_fake", self.avg_prob_fake)
+        tf.scalar_summary("avg_probs_real", self.avg_prob_real)
+        tf.scalar_summary("d_loss_real", self.d_loss_real)
+        tf.scalar_summary("d_loss_fake", self.d_loss_fake)
+
+        tf.scalar_summary("d_loss", self.d_loss)
+        tf.scalar_summary("g_loss", self.g_loss)
+        tf.scalar_summary("total_loss", self.total_loss)
+
+        self.merged_summary = tf.merge_all_summaries()
+
+        t_vars = tf.trainable_variables()
+
+        self.d_vars = [var for var in t_vars if 'd_' in var.name]
+        self.g_vars = [var for var in t_vars if 'g_' in var.name]
+
+        self.saver = tf.train.Saver()
+
+    def train(self, config):
+
+        d_optim = tf.train.AdamOptimizer(config.d_learning_rate, beta1=config.beta1).minimize(self.d_loss, var_list=self.d_vars)
+        g_optim = tf.train.AdamOptimizer(config.g_learning_rate, beta1=config.beta1).minimize(self.g_loss, var_list=self.g_vars)
+
+        tf.global_variables_initializer().run()
+        self.writer = tf.train.SummaryWriter(config.result_dir + 'log/' + config.dataset + '_' + config.dir_tag, self.sess.graph)
+
+        data = DataProvider(config)
+
+        sample_images = data.load_data(config, 0)
+        sample_z = np.random.uniform(-1, 1, size=(config.batch_size, config.z_dim))
+
+        save_size = int(math.sqrt(config.batch_size))
+        save_images(sample_images[:save_size * save_size], [save_size, save_size], '{}/train_{:02d}_{:04d}.png'.format(config.sample_dir, 0, 0))
+
+        if config.b_loadcheckpoint:
+            if self.load(config):
+                print(" [*] Load SUCCESS")
+            else:
+                print(" [!] Load failed...")
+
+        counter = 1
+        start_time = time.time()
+
+        for epoch in xrange(config.epoch):
+
+            batch_idxs = min(data.len, config.train_size) // config.batch_size
+            for idx in xrange(0, batch_idxs):
+
+                batch_images = data.load_data(config, idx)
+                batch_z = np.random.uniform(-1, 1, [config.batch_size, config.z_dim]).astype(np.float32)
+
+                # Update D network
+                for k_d in xrange(0, config.K_for_Dtrain):
+                    _, summary_str, _loss, _prob_real, _prob_fake = self.sess.run([d_optim, self.merged_summary, self.total_loss, self.avg_prob_real, self.avg_prob_fake], feed_dict={self.z: batch_z, self.images: batch_images})
+                    self.writer.add_summary(summary_str, counter)
+                    # when running d_optim, basically the whole graph will be executed, so, we get summary and log info from here.
+
+                # Update G network
+                for k_g in xrange(0, config.K_for_Gtrain):
+                    self.sess.run([g_optim], feed_dict={self.z: batch_z})
+
+                counter += 1
+
+                print("Epoch: [%2d] [%4d/%4d] time: %4.4f, loss: %.8f, prob_real: %.8f, prob_fake: %.8f" % (
+                    epoch, idx, batch_idxs, time.time() - start_time, _loss, _prob_real, _prob_fake))
+
+                if np.mod(counter, 100) == 1:
+                    _generate_image, _loss, _prob_real, _prob_fake = self.sess.run([self.generate_image, self.total_loss, self.avg_prob_real, self.avg_prob_fake], feed_dict={self.z: sample_z, self.images: sample_images})
+
+                    save_size = int(math.sqrt(config.batch_size))
+                    save_images(_generate_image[:save_size * save_size], [save_size, save_size], '{}/train_{:02d}_{:04d}.png'.format(config.sample_dir, epoch, idx))
+                    print("[Sample] loss: %.8f, prob_real: %.8f, prob_fake: %.8f" % (_loss, _prob_real, _prob_fake))
+
+                if np.mod(counter, 500) == 2:
+                    self.save(config, counter)
+
+    def discriminator(self, image, y=None, reuse=False, config=None):
+        if reuse:
+            tf.get_variable_scope().reuse_variables()
+
+        h0 = lrelu(conv2d(image, config.df_dim, name='d_h0_conv'))
+        h1 = lrelu(batch_norm(conv2d(h0, config.df_dim * 2, name='d_h1_conv'), 'd_bn1'))
+        h2 = lrelu(batch_norm(conv2d(h1, config.df_dim * 4, name='d_h2_conv'), 'd_bn2'))
+        h3 = lrelu(batch_norm(conv2d(h2, config.df_dim * 8, name='d_h3_conv'), 'd_bn3'))
+        h4 = linear(tf.reshape(h3, [config.batch_size, -1]), 1, 'd_h3_lin')
+
+        return tf.nn.sigmoid(h4), h4
+
+    def generator(self, z, config=None):
+
+        print('Generator shape')
+        print('z : ', z.get_shape())
+
+        curfdim = config.gf_dim
+        cursize = config.image_size // 2
+        while (cursize % 2 == 0 and cursize >= 8):
+            cursize = cursize // 2
+            curfdim *= 2
+
+        zr = linear(z, curfdim * cursize * cursize, 'g_h0_lin', with_w=False)
+        h0 = tf.reshape(zr, [-1, cursize, cursize, curfdim])
+        h0 = tf.nn.relu(batch_norm(h0, "g_vbn_0"))
+        print('h0: ', h0.get_shape())
+
+        idx = 1
+        while config.image_size // 2 != cursize:
+
+            idx += 1
+            cursize *= 2
+            curfdim //= 2
+
+            h0 = deconv2d(h0, [config.batch_size, cursize, cursize, curfdim], name='g_h' + str(idx), with_w=False)
+            h0 = tf.nn.relu(batch_norm(h0, "g_vbn_" + str(idx)))
+            print('h' + str(idx) + ': ', h0.get_shape())
+
+        h0 = deconv2d(h0, [config.batch_size, config.image_size, config.image_size, config.c_dim], name='g_h' + str(idx+1), with_w=False)
+        out = tf.nn.tanh(h0)
+        print('out:', out.get_shape())
+
+        return out
+
+    def generator2(self, z, config=None):
+        s = config.image_size
+        s2, s4, s8, s16 = int(s / 2), int(s / 4), int(s / 8), int(s / 16)
+
+        # project `z` and reshape
+        zr, self.h0_w, self.h0_b = linear(z, config.gf_dim * 8 * s16 * s16, 'g_h0_lin', with_w=True)
+
+        h0 = tf.reshape(self.zr, [-1, s16, s16, config.gf_dim * 8])
+        h0 = tf.nn.relu(batch_norm(h0,'g_bn0'))
+
+        h1, self.h1_w, self.h1_b = deconv2d(h0, [config.batch_size, s8, s8, config.gf_dim * 4], name='g_h1', with_w=True)
+        h1 = tf.nn.relu(batch_norm(h1,'g_bn1'))
+
+        h2, self.h2_w, self.h2_b = deconv2d(h1, [config.batch_size, s4, s4, config.gf_dim * 2], name='g_h2', with_w=True)
+        h2 = tf.nn.relu(batch_norm(h2,'g_bn2'))
+
+        h3, self.h3_w, self.h3_b = deconv2d(h2, [config.batch_size, s2, s2, config.gf_dim * 1], name='g_h3', with_w=True)
+        h3 = tf.nn.relu(batch_norm(h3,'g_bn3'))
+
+        h4, self.h4_w, self.h4_b = deconv2d(h3, [config.batch_size, s, s, config.c_dim], name='g_h4', with_w=True)
+
+        return tf.nn.tanh(h4)
+
+    def save(self, config=None, step=0):
+        model_name = "DCGAN.model"
+        model_dir = "%s_%s_%s" % (config.dataset, config.batch_size, config.image_size)
+        checkpoint_dir = os.path.join(config.checkpoint_dir, model_dir)
+
+        if not os.path.exists(checkpoint_dir):
+            os.makedirs(checkpoint_dir)
+
+        self.saver.save(self.sess, os.path.join(checkpoint_dir, model_name), global_step=step)
+
+    def load(self, config=None):
+        print(" [*] Reading checkpoints...")
+
+        model_dir = "%s_%s_%s" % (config.dataset, config.batch_size, config.image_size)
+        checkpoint_dir = os.path.join(config.checkpoint_dir, model_dir)
+
+        ckpt = tf.train.get_checkpoint_state(checkpoint_dir)
+        if ckpt and ckpt.model_checkpoint_path:
+            ckpt_name = os.path.basename(ckpt.model_checkpoint_path)
+            self.saver.restore(self.sess, os.path.join(checkpoint_dir, ckpt_name))
+            return True
+        else:
+            return False
