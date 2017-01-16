cbb17ac0f94411aebc94439afeea7d1362b7b06b
ccyyatnet <1124015396@qq.com>
2017-01-16 13:33:33 +0800

    update

--- model.py
+++ model.py
@@ -362,20 +362,23 @@ class DCGAN(object):
         print 'h0 shape after concat:', h0.get_shape()
         h0 = tf.nn.relu(batch_norm(h0, 'g_bn0'))
 
-        h1 = conv2d(h0, 32, d_h = 1, d_w = 1, name = 'g_h1_conv')
+        h1 = conv2d(h0, 128, k_h = 7, k_w = 7, d_h = 1, d_w = 1, name = 'g_h1_conv')
         h1 = tf.nn.relu(batch_norm(h1, 'g_bn1'))
 
-        h2 = conv2d(h1, 64, d_h = 1, d_w = 1, name = 'g_h2_conv')
+        h2 = conv2d(h1, 64, k_h = 5, k_w = 5, d_h = 1, d_w = 1, name = 'g_h2_conv')
         h2 = tf.nn.relu(batch_norm(h2, 'g_bn2'))
 
-        h3 = conv2d(h2, 64, d_h = 1, d_w = 1, name = 'g_h3_conv')
+        h3 = conv2d(h2, 64, k_h = 5, k_w = 5, d_h = 1, d_w = 1, name = 'g_h3_conv')
         h3 = tf.nn.relu(batch_norm(h3, 'g_bn3'))
 
-        h4 = conv2d(h3, 32, d_h = 1, d_w = 1, name = 'g_h4_conv')
+        h4 = conv2d(h3, 64, k_h = 5, k_w = 5,  d_h = 1, d_w = 1, name = 'g_h4_conv')
         h4 = tf.nn.relu(batch_norm(h4, 'g_bn4'))
 
-        h5 = conv2d(h4, 2, d_h = 1, d_w = 1, name = 'g_h5_conv')
-        out = tf.nn.tanh(h5)
+        h5 = conv2d(h4, 32, k_h = 5, k_w = 5,  d_h =1, d_w = 1, name = 'g_h5_conv')
+        h5 = tf.nn.relu(batch_norm(h5, 'g_bn5'))
+
+        h6 = conv2d(h5, 2, k_h = 5, k_w = 5,  d_h = 1, d_w = 1, name = 'g_h6_conv')
+        out = tf.nn.tanh(h6)
 
         print 'generator out shape:', out.get_shape()
 
